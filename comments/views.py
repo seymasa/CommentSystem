c8c34d2fac8044c2855379a46e489b23b8326661
Seyma SARIGIL <seymasargil@gmail.com>
2017-10-19 17:52:49 +0300

    Failed js

--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
+from django.shortcuts import HttpResponse,render, HttpResponseRedirect, get_object_or_404
+import json
 from django.urls import reverse
 from .models import Post, Comment
 from datetime import datetime
@@ -15,4 +16,9 @@ def comment(request,post_id):
     #hide_user = request(default={'hide_user': False})
     comment = Comment(content=request.POST.get("content"), user=user, post=post,publishDate=now)
     comment.save()
-    return HttpResponseRedirect(reverse('comment.index'))
+    cevap = {}
+    cevap['status'] = True
+    cevap['comment'] = request.POST.get('content')
+    cevap['publishDate'] = comment.publishDate.strftime("%d.%m.%Y");
+    cevap['user'] = comment.user.username
+    return HttpResponse(json.dumps(cevap), content_type="application/json")
