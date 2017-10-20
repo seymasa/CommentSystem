9be8d2112b83bf4fbeb2a359ff155058f53672f4
Seyma SARIGIL <seymasargil@gmail.com>
2017-10-20 14:12:58 +0300

    matl

--- static/js/all.js
+++ static/js/all.js
@@ -1,15 +1,10 @@
-$(".ajax-form").submit(function(){
-    var url = $(this).attr("action");
-    var data = $(this).serialize();
-    $.ajax({
-    method: "POST",
-    url: url,
-    data: data,
-    dataType: "JSON"
-    }).done(function(sonuc){
-
-    });
-    $(this).find("textarea").prop("disabled", true);
-    $(this).find("button").html("Gönderiliyor...");
-    return false;
+var url = $(this).attr("action");
+var data = $(this).serialize();
+$.ajax({
+method: "POST",
+url: url,
+data: data,
+dataType: "JSON"
+}).done(function(sonuc){
+alert(sonuc);
 });
