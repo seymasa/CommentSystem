9ea674e1a0a9cfc6f8e50773a67c8f8aabb585ac
Seyma SARIGIL <seymasargil@gmail.com>
2017-10-24 16:24:13 +0300

    Security Hight Level ON

--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import HttpResponse,render, HttpResponseRedirect, get_object_or_404
-import json
-from django.urls import reverse
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
 from .models import Post, Comment
 from datetime import datetime
 # Create your views here.
@@ -10,21 +9,37 @@ def index(request):
 
 
 def comment(request, post_id):
-    cevap = {}
-    if (request.POST.get("content") == ""):
-        cevap['status'] = False
-        cevap['error'] = "Please enter a comment ..."
-    else:
-        now = datetime.now()
-        user = request.user
-        post = get_object_or_404(Post, pk=post_id)
-        # hide_user = request(default={'hide_user': False})
-        comment = Comment(content=request.POST.get("content"), user=user, post=post, publishDate=now)
-        comment.save()
-        cevap['status'] = True
-        cevap['comment'] = request.POST.get('content')
-        cevap['publishDate'] = comment.publishDate.strftime("%d.%m.%Y %H:%M")
-        cevap['user'] = comment.user.username
+    post = get_object_or_404(Post, pk=post_id)
+    if request.POST.get("content") == "":
+        return JsonResponse({'status': False,
+                             'error': "Please enter a comment ..."})
 
-    return HttpResponse(json.dumps(cevap), content_type="application/json")
+    time_control = datetime.now() - post.comment_set.filter(user_id=request.user.id).order_by(Comment.publishDate)[0].publishDate
+    if time_control.seconds <= 15:
+        return JsonResponse({'status': False,
+                             'error': "Try commenting again in 15 seconds!"})
 
+    content = request.POST.get("content")
+    comment = Comment(content=content, user=request.user, post=post, publishDate=datetime.now())
+    comment.save()
+    return JsonResponse({'status': True,
+                         'comment': content,
+                         'publishDate': comment.publishDate.strftime("%d.%m.%Y %H:%M"),
+                         'user': comment.user.username})
+
+'''
+Bana Gelen Veriler:
+- post_id
+- user
+
+Yapılacak İşlemler
+1) post_id'yi kullanarak post modeline eriş
+2) o post modeline ait yorumlardan user_id'si bana gelen user olanın yorumlarını tarihe göre büyükten küçüğe sırala ve ilkini çek
+3) çekebildim mi?
+ 3.a) çekebildiysem o yorumun tarihi ile benim şuanki tarih saatimi karşılaştır ve uygun mu bak?
+   3.a.1) uygun değilse hata döndür
+   3.a.2) uygunsa devam et
+  3.b) çekemediysem bir şeyi kontrol etmeye gerek yok yorumu kaydetmeye devam et
+4) yeni yorumu kaydet
+1085165	IBAN TR30 00 06 40 00 00 12 50 01 08 51 65
+'''
